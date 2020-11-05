53ade23366203c1bac27451040b58392b74ffba4
bambi teague <teeegsa@gmail.com>
2020-11-04 21:06:02 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+# Create a class, School, in the lib directory that can be initialized with a name
+require 'pry'
+
+class School
+
+    attr_accessor :roster
+    attr_accessor :grade
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def name
+        @name
+    end
+   
+    def add_student(name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []  
+        end  
+        @roster[grade] << name  
+    end
+
+    def grade(num)
+        roster.detect do |x, y| 
+            if x == num
+            return y 
+            end 
+        end 
+    end
+    
+    def sort 
+        new_hash = {}
+        roster.each do |x, y| 
+          new_hash[x] = y.sort 
+        end 
+        new_hash
+      end 
+
+end
